e4506a0c384dab68ca9e3859d0f76590c6521c93
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-08-19 03:52:51 +0100

    gg

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import BoardContainer from "@/components/BoardContainer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-20 items-center justify-center h-screen container">
+    <main className="flex flex-col px-10 items-center justify-center h-screen container">
       <BoardContainer />
     </main>
   );
